c4e199c29c53c0868e3e158e0c95d5322d37cbf3
Dedoff1 <128306784+Dedoff1@users.noreply.github.com>
2025-02-17 18:14:44 +0300

    Removed some lines

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -1,4 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Test? Additional stuff");
-Console.WriteLine("Hello, Test? Additional stuff");
-Console.WriteLine("Hello, Test? Additional stuff");
